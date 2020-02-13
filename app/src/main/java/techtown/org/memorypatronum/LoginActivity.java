d591eccbe5a313e48e1cd93c282692f78917f09b
gnnouoemh <sungmoon1202@naver.com>
2020-02-13 19:06:12 +0900

    Login 추가

--- app/src/main/java/techtown/org/memorypatronum/LoginActivity.java
+++ app/src/main/java/techtown/org/memorypatronum/LoginActivity.java
@@ -1,21 +1,179 @@
 package techtown.org.memorypatronum;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import techtown.org.memorypatronum.MainActivity;
+import techtown.org.memorypatronum.R;
+
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static String IP_ADDRESS = "192.168.219.177";
+    private static String TAG = "phptest";
+
+
+    private EditText mEditTextID;
+    private EditText mEditTextPassword;
+    private TextView mTextViewResult;
+
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @SuppressWarnings("unused")
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEditTextID = (EditText)findViewById(R.id.editText_main_id);
+        mEditTextPassword = (EditText)findViewById(R.id.editText_main_password);
+        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+
+        mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+
+        Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @SuppressWarnings("unused")
+            public void onClick(View v) {
+
+                String id = mEditTextID.getText().toString();
+                String password = mEditTextPassword.getText().toString();
+
+                InsertData task = new InsertData();
+                task.execute("http://" + IP_ADDRESS + "/new.php", id, password);
+
+
+                mEditTextID.setText("");
+                mEditTextPassword.setText("");
+
+
+            }
+        });
+
     }
 
-    public void onTempClick(View view){ //main화면으로 넘어가는 임시버튼. 일단은 지우지 말기.
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+
+
+    class InsertData extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialog;
+
+        @Override
+        @SuppressWarnings("unused")
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(LoginActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        @SuppressWarnings("unused")
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            //mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+            if(result.equals("로그인 실패")){
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                mEditTextID.setText("");
+                mEditTextPassword.setText("");
+            }
+            else{
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }
+
+
+        @Override
+        @SuppressWarnings("unused")
+        protected String doInBackground(String... params) {
+
+            String id = (String)params[1];
+            String password = (String)params[2];
+
+            String serverURL = (String)params[0];
+            String postParameters = "&id=" + id + "&password=" + password;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
     }
+
+
 }
